97ff9707f17dafd6406e9dc702ef609c179396d6
Vu Tran Trung <vutt1808@gmail.com>
2025-09-19 10:58:55 +0700

    store configuration

---
+++ src/stores/index.ts
@@ -0,0 +1,39 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+
+const rootReducer = combineReducers({
+    // more reducers go here
+    // user: userSlice
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: [
+        // reducers you want to persist here
+    ],
+    blacklist: [
+        // don't persist
+    ]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Store configuration
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleWare) => 
+        getDefaultMiddleWare({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+        devTools: process.env.NODE_ENV !== 'production'
+})
+
+export const persistor = persistStore(store)
+
+// Types
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
