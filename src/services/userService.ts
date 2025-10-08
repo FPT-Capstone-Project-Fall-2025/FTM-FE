889af94505ddd71f308838f4453ce4423112cced
Vu Tran <157681421+trungvuu188@users.noreply.github.com>
2025-10-08 20:17:05 +0700

    Integrate: update profile & change-password(testing) apis  (#18)
    * integrate update avatar & bugs fixed api
    
    * fix: pro build
    
    * integrate update avatar & bugs fixed api
    
    * fix: pro build
    
    * fix: ward is not change when editing
    
    * testing: integrate change password api
    
    ---------
    
    Co-authored-by: Vu Tran <vu.tran-trung@warwick.ac.uk>

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+import type { ChangePasswordProps } from './../types/user';
 import type { ApiResponse } from '../types/api';
 import type { AvatarUpdateResponse, EditUserProfile, UserProfile } from '@/types/user';
 import api from './apiService';
@@ -19,6 +20,10 @@ const userService = {
         "Content-Type": "multipart/form-data"
       }
     });
+  },
+
+  changePassword(props: ChangePasswordProps): Promise<ApiResponse<boolean>> {
+    return api.put('/account/change-password', { ...props });
   },
 };
 
